d1e3aba677348a3b9b3e6d1a417688ee3bac3176
samory harris <samory070@gmail.com>
2018-03-05 21:46:39 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,8 @@
 # Define the variable board below.
 
-puts row1 = [" ", " ", " "]
-puts row2 = [" ", " ", " "]
-puts row3 = [" ", " ", " "]
+row1 = [" ", " ", " "]
+row2 = [" ", " ", " "]
+row3 = [" ", " ", " "]
 
 row[1] = 'X'
    |   |
